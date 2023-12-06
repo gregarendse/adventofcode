3e0e23ad7ba6c06a7b560c6fb8153efd51fbc194
Greg Arendse <greg.arendse@standardbank.co.za>
2023-12-06 11:25:19 +0000

    Day 3: Gear Ratios

--- 2023/3/main.py
+++ 2023/3/main.py
@@ -47,7 +47,6 @@ def get_points(i: int = 0) -> List[Tuple[int, int]]:
 
     # Top & Bottom
     for dx in range(i):
-        # for (ddx, ddy) in [(1, 0), (1, 1), (0, 1), (-1, 1), (-1, 0), (-1, -1), (0, -1), (1, -1)]:
         for (ddx, ddy) in [(0, 1), (0, -1)]:
             points.add((ddx + dx, ddy))
     return sorted(list(points))
@@ -55,70 +54,112 @@ def get_points(i: int = 0) -> List[Tuple[int, int]]:
 
 def __part_one__(file: Path) -> int:
     lines = read_from_file(file)
-    characters: Dict[str, int] = defaultdict(int)
-
     part_numbers = []
 
     for y, line in enumerate(lines):
         x: int = 0
         while x < len(line):
-            characters[line[x]] += 1
-
-            dx: int = 1
-            while x + dx + 1 < len(line) and str(line[x:x + dx + 1]).isnumeric():
+            dx: int = 0
+            while x + dx + 1 <= len(line) and str(line[x:x + dx + 1]).isnumeric():
                 dx += 1
+
+            if dx == 0:
+                x += 1
+                continue
 
             if str(line[x:x + dx]).isnumeric():
                 n = int(line[x:x + dx])
 
                 points = get_points(dx)
-                print(f"{n} -> {points}")
+
+                around = []
 
                 for (ddx, ddy) in points:
-                    if 0 <= y + ddy < len(lines):
+                    print(f"{x + ddx},{y + ddy}")
+                    if 0 <= y + ddy < len(lines) and 0 <= x + ddx < len(lines[y + ddy]):
                         pass
                     else:
                         continue
 
-                    if 0 <= x + ddx < len(lines[y + ddy]):
-                        pass
-                    else:
+                    c: str = str(lines[y + ddy][x + ddx])
+
+                    around.append(c)
+
+                    if c == '.' or c.isnumeric():
                         continue
 
-                    c = lines[y + ddy][x + ddx]
-
-                    if str(c).isnumeric():
-                        continue
-
-                    if c != '.':
-                        part_numbers.append(n)
-                        break
+                if len(list(filter(lambda x: str(x) != '.' and not str(x).isnumeric(), around))) > 0:
+                    part_numbers.append(n)
+                print(f"{n} -> {around}")
 
             x += dx
 
-    for k, v in characters.items():
-        print(f"{k} {v}")
     return sum(part_numbers)
 
 
 def __part_two__(file: Path) -> int:
-    raise Exception("Not implemented")
-    content: str = read_from_file(file)
-    return 0
+    lines = read_from_file(file)
+    gears: Dict[Tuple[int, int], List[int]] = defaultdict(list)
+
+    for y, line in enumerate(lines):
+        x: int = 0
+        while x < len(line):
+            dx: int = 0
+            while x + dx + 1 <= len(line) and str(line[x:x + dx + 1]).isnumeric():
+                dx += 1
+
+            if dx == 0:
+                x += 1
+                continue
+
+            if str(line[x:x + dx]).isnumeric():
+                n = int(line[x:x + dx])
+
+                points = get_points(dx)
+
+                around = []
+
+                for (ddx, ddy) in points:
+                    if 0 <= y + ddy < len(lines) and 0 <= x + ddx < len(lines[y + ddy]):
+                        pass
+                    else:
+                        continue
+
+                    c: str = str(lines[y + ddy][x + ddx])
+
+                    if c == '*':
+                        gears[(x + ddx, y + ddy)].append(n)
+                        break
+
+            x += dx
+
+    total: int = 0
+
+    for part_numbers in gears.values():
+        if len(part_numbers) != 2:
+            continue
+        ans: int = 1
+        for part_number in part_numbers:
+            ans *= part_number
+        total += ans
+
+    return total
 
 
 def part_one() -> int:
     example: int = __part_one__(PARENT.joinpath('example.txt'))
     assert example == 4361
+    example: int = __part_one__(PARENT.joinpath('example_2.txt'))
+    assert example == 9 + 633 + 63
     answer: int = __part_one__(PARENT.joinpath('input.txt'))
     return answer
 
 
 def part_two() -> int:
-    # example: int = __part_two__(PARENT.joinpath('example.txt'))
-    # assert example == 0
-    # answer: int = __part_two__(PARENT.joinpath('input.txt'))
-    return None
+    example: int = __part_two__(PARENT.joinpath('example.txt'))
+    assert example == 467835
+    answer: int = __part_two__(PARENT.joinpath('input.txt'))
+    return answer
 
 
 def main():
